9f9e2d708aa2cb98efc0d9329d768341b5588aeb
Ding Wei <daviding924@users.noreply.github.com>
2014-12-02 11:47:23 +0800

    Update setup_ubuntu_build_env.sh
    add install openjdk 7
    create switch JDK link
    change ccache size

--- setup/setup_ubuntu_build_env.sh
+++ setup/setup_ubuntu_build_env.sh
@@ -33,8 +33,8 @@ if [[ $USER != root ]] ; then
 	exit 0
 fi
 
-mkdir -p /local/{ccache,ref_repo}
-chmod 777 /local /local/{ccache,ref_repo}
+mkdir -p /local/{ccache,ref_repo,out}
+chmod 777 /local /local/{ccache,ref_repo,out}
 chmod +s /sbin/btrfs*
 
 mkdir -p /root/.ssh
@@ -57,7 +57,7 @@ libncurses5-dev libncursesw5-dev ncurses-hexedit openssh-server \
 gcc-4.2 g++-4.2 libstdc++5 libstdc++6-4.2 automake1.8 automake1.9 mkisofs \
 build-essential libz-dev flex gperf libwxgtk2.6-dev libcurses-widgets-perl \
 libcurses-perl libcurses-ui-perl libcurses-ruby libcurses-ruby1.8 \
-libsdl-dev ncurses-dev libtool sun-java6-jdk python-software-properties \
+libsdl-dev ncurses-dev libtool python-software-properties \
 cramfsprogs libx11-dev ncurses-term python-soappy xlockmore python-lxml \
 mingw32 tofrodos libc6-dev-i386 lib32z1-dev lib32ncurses5-dev \
 libzzip-dev libc6-dev-amd64 g++-multilib lib64stdc++6 lib64z1-dev \
@@ -79,6 +79,9 @@ x2goserver x2goclient wmii2 dwm wmctrl xfce4
 # setup version control tool
 aptitude -y install git git-core tig subversion subversion-tools \
 python-svn libsvn-perl
+# setup openjdk 7 for AOSP L build
+# setup Sun JDK 1.6 for AOSP build before L
+aptitude -y install openjdk-7-jdk sun-java6-jdk
 
 svn co $SVN_OPTION -q $SVN_SRV/tools/tools /local/tools
 export RUN_PATH=/local/tools/setup
@@ -89,9 +92,12 @@ export RUN_PATH=/local/tools/setup
 ln -sf /usr/lib/jvm/java-6-sun /usr/local/jdk1.6
 ln -s /usr/bin/fromdos /usr/local/bin/dos2unix
 
-echo "PATH=/usr/local/jdk1.6/bin:\$PATH:
-CLASSPATH=/usr/local/jdk1.6/lib:.
-JAVA_HOME=/usr/local/jdk1.6
+ln -sf /usr/lib/jvm/java-7-openjdk-amd64 /usr/local/jdk1.7
+ln -sf /usr/local/jdk1.6 /usr/local/jdk
+
+echo "PATH=/usr/local/jdk/bin:\$PATH:
+CLASSPATH=/usr/local/jdk/lib:.
+JAVA_HOME=/usr/local/jdk
 export PATH CLASSPATH JAVA_HOME
 export USE_CCACHE=1
 export CCACHE_UMASK=0000
@@ -111,16 +117,16 @@ if [[ ! `grep ibuild /etc/bash.bashrc` ]] ; then
 fi
 
 alias ccache=/usr/bin/ccache
-ccache -M 20G
+ccache -M 50G
 
 echo '
 Our suggestion:
 Disable HT(Hyper-Threading) in BIOS
 run "lshw -c disk" check your disk
-Setup Ubuntu 12.04 x86_64 / in HDD 30G with ext4
+Setup Ubuntu 14.04 x86_64 / in HDD 50G with ext4
 Setup 4G swap
 Setup /home in HDD with free space btrfs
-Setup /local in SSD with whole space btrfs
+Setup /local in SSD with whole space ext4
 Shared /home/ref_repo
 Use your workspace in /home/$USER/My...
 Shared /local/ccache
