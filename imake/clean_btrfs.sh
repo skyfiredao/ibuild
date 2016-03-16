f531a807a66e30e36c4235e34dd51b2c8eef0ef7
Ding Wei <daviding924@users.noreply.github.com>
2016-03-16 10:16:11 +0800

    dw: change REF_REPO to SUBV_REPO for all of tools

--- imake/clean_btrfs.sh
+++ imake/clean_btrfs.sh
@@ -38,19 +38,19 @@ if [[ -z $1 ]] ; then
 	exit 0
 fi
 
-CLEAN_REF_REPO()
+CLEAN_SUBV_REPO()
 {
- for REF_REPO_MD5 in `ls $LOC_WORKSPACE/ref_repo | egrep -v 'info'`
+ for SUBV_REPO_MD5 in `ls $LOC_WORKSPACE/subv_repo | egrep -v 'info'`
  do
-	if [[ ! -f $LOC_WORKSPACE/ref_repo/$REF_REPO_MD5/Makefile || `cat $LOC_WORKSPACE/ref_repo/$REF_REPO_MD5.info | grep mirror` ]] ; then
+	if [[ ! -f $LOC_WORKSPACE/subv_repo/$SUBV_REPO_MD5/Makefile || `cat $LOC_WORKSPACE/subv_repo/$SUBV_REPO_MD5.info | grep mirror` ]] ; then
 		export SEED=$RANDOM
 		echo -------------------------
-		echo $LOC_WORKSPACE/ref_repo/$REF_REPO_MD5
+		echo $LOC_WORKSPACE/subv_repo/$SUBV_REPO_MD5
 		echo $LOC_WORKSPACE/build/bad.$SEED
-		cat $LOC_WORKSPACE/ref_repo/$REF_REPO_MD5.info
+		cat $LOC_WORKSPACE/subv_repo/$SUBV_REPO_MD5.info
 		echo -------------------------
-		mv $LOC_WORKSPACE/ref_repo/$REF_REPO_MD5 $LOC_WORKSPACE/build/bad.$SEED
-		mv $LOC_WORKSPACE/ref_repo/$REF_REPO_MD5.info $LOC_WORKSPACE/build/bad.$SEED/
+		mv $LOC_WORKSPACE/subv_repo/$SUBV_REPO_MD5 $LOC_WORKSPACE/build/bad.$SEED
+		mv $LOC_WORKSPACE/subv_repo/$SUBV_REPO_MD5.info $LOC_WORKSPACE/build/bad.$SEED/
 	fi
  done
 }
@@ -59,7 +59,7 @@ CLEAN_STEPS()
 {
  echo "sudo btrfs subvolume list $LOC_WORKSPACE"
 
- for BTRFS_SUBVOL in `sudo btrfs subvolume list $LOC_WORKSPACE | egrep -v 'ref_repo' | awk -F'path ' {'print $2'} | awk -F'build/' {'print $2'}`
+ for BTRFS_SUBVOL in `sudo btrfs subvolume list $LOC_WORKSPACE | egrep -v 'subv_repo' | awk -F'path ' {'print $2'} | awk -F'build/' {'print $2'}`
  do
 	sleep 3
 	echo "sudo btrfs subvolume delete $LOC_WORKSPACE/$BTRFS_SUBVOL"
@@ -68,7 +68,7 @@ CLEAN_STEPS()
 }
 
 if [[ ! -f $TASK_SPACE/spec.build ]] ; then
-	CLEAN_REF_REPO
+	CLEAN_SUBV_REPO
 	CLEAN_STEPS
 else
 	echo $TASK_SPACE/spec.build
--- setup/README.md
+++ setup/README.md
@@ -11,7 +11,7 @@ non-stand-alone tool, build node deamon listen ibuild maseter send job
 stand-alone tool, monitor ccache status, out and ccache disk space and CPU, display card temperature
 
 #### ibuild_node_reg.sh
-stand-alone tool, every 5 or 10min run in all of ibuild node for update node info, sync local mirror, auto cleanup and auto reboot
+stand-alone tool, every 5 or 10min run in all of ibuild node for update node info, sync local ref_repo, auto cleanup and auto reboot
 
 #### reboot.sh
 stand-alone tool, reboot node after check count
@@ -20,8 +20,8 @@ stand-alone tool, reboot node after check count
 stand-alone tool, init setup ibuild node after install ubuntu basic OS
 
 #### sync_node_setup.sh
-stand-alone tool, setup ibuild node local mirror for build after standard ubuntu setup
+stand-alone tool, setup ibuild node local ref_repo for build after standard ubuntu setup
 
-#### sync_repo_local_mirror.sh
+#### sync_local_ref_repo.sh
 stand-alone tool, sync local mirror in ibuild node
 
