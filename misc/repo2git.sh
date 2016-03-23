8e716a0bbb5df703e1f415db2030d48617ad5cd8
Ding Wei <daviding924@users.noreply.github.com>
2016-03-23 08:40:48 +0800

    dw: fix filter issue

--- misc/repo2git.sh
+++ misc/repo2git.sh
@@ -21,7 +21,7 @@ export REPO_PATH=$1
 export GIT_PATH=$2
 export NOW=$(date +%y%m%d%H%M%S)
 export LOC_WS=$(dirname $GIT_PATH)
-[[ `echo $* | grep debug` ]] && export DEBUG=echo
+[[ `echo $* | egrep ' debug'` ]] && export DEBUG=echo
 
 [[ ! -d $REPO_PATH/.repo || ! -d $GIT_PATH/.git ]] && exit 1
 
@@ -42,7 +42,7 @@ echo "tail -f /tmp/repo2git.log"
 rsync -av --delete $REPO_PATH/ $GIT_PATH/ >/tmp/repo2git.log 2>&1
 
 cd $GIT_PATH
-for CLEAN in `cat $LOC_WS/$NOW/file_repo.list | egrep '.git$|.gitignore$|.svn$|.gitattributes$|speechdata'`
+for CLEAN in `cat $LOC_WS/$NOW/file_repo.list | egrep '\.git$|\.gitignore$|\.svn$|\.gitattributes$|speechdata'`
 do
     rm -fr $GIT_PATH/$CLEAN
 done
@@ -59,13 +59,19 @@ mkdir -p external/chromium_org/third_party/angle/git
 cp $REPO_PATH/external/chromium_org/third_party/angle/.git/index $GIT_PATH/external/chromium_org/third_party/angle/git/
 echo "cp -R external/chromium_org/third_party/angle/git external/chromium_org/third_party/angle/.git" >>$GIT_PATH/build/envsetup.sh
 
-rm -fr cts docs prebuilts/eclipse external/eclipse-basebuilder tools/external/gradle
+rm -fr cts docs \
+prebuilts/eclipse \
+external/eclipse-basebuilder \
+tools/external/gradle external/chromium_org/chrome/common/extensions/docs/server2/test_data/github_file_system/apps_samples.zip \
+frameworks/base/docs/downloads/design/*.zip \
+prebuilts/misc/common/groovy/*.zip \
+vendor/auto/projection_protocol/receiver-lib/receiver-lib-docs.zip
 
 mv $LOC_WS/$NOW/git $GIT_PATH/.git
 
 git status >$LOC_WS/$NOW/git.status 2>&1
 
-git add $GIT_PATH/external/chromium_org/third_party/angle/git
+git add $GIT_PATH/external/chromium_org/third_party/angle/git >/dev/null 2>&1
 
 for GIT_ADD in `cat $LOC_WS/$NOW/git.status | grep '^#' | egrep 'deleted|modified' | awk -F' ' {'print $3'} | awk -F'/' {'print $1'} | sort -u`
 do
@@ -84,7 +90,8 @@ $DEBUG        git push
     done
 done
 
-for GIT_IGNORE in `cat $LOC_WS/$NOW/file_repo.list | egrep '.git$|.gitignore$|.gitattributes$'`
+#|.gitignore$
+for GIT_IGNORE in `cat $LOC_WS/$NOW/file_repo.list | egrep '.git$|.gitattributes$'`
 do
     export GIT_IGNORE_PATH=`dirname $GIT_IGNORE`
     [[ -d $GIT_IGNORE ]] && cp $GIT_PATH/$GIT_IGNORE $REPO_PATH/$GIT_IGNORE
@@ -104,7 +111,7 @@ $DEBUG        git add $GIT_PATH/$GIT_ADD
 done
 
 $DEBUG git add *
-$DEBUG git commit -m "auto commit gitignore" *
+$DEBUG git commit -m "auto commit missed files" *
 $DEBUG git push
 
 mv $LOC_WS/$NOW/repo $REPO_PATH/.repo
