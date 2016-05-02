ac73cfff5f4dc009f5aa977de5154c20a5404c00
Ding Wei <dingwei@ding.wei>
2016-05-02 15:54:22 -0400

    dw: change debug tool with debug_ name

--- setup/debug_check_env.sh
+++ setup/debug_check_env.sh
@@ -34,29 +34,25 @@ ECHO()
 [[ ! `ccache -V | grep 3.2` ]] && ECHO 'no ccache 3.2'
 [[ ! -f ~/.gitconfig ]] && ECHO 'no ~/.gitconfig'
 [[ ! -f ~/.ssh/id_rsa-irobot ]] && ECHO 'no irobot ssh key'
-[[ ! -d ~/bin/binfile ]] && ECHO 'no ~/bin/binfile' 
 [[ ! `mount | grep btrfs` ]] && ECHO 'no btrfs'
-[[ ! `cat /etc/fstab | grep ccache | grep noatime` ]] && ECHO 'no SSD ccache'
-[[ ! `cat /etc/fstab | grep out | grep noatime` ]] && ECHO 'no SSD out'
+[[ ! `df | grep ccache$` ]] && ECHO 'no ccache'
+[[ ! `df | grep out$` ]] && ECHO 'no out'
 [[ ! `grep ibuild /etc/ganglia/gmond.conf` ]] && ECHO 'no right ganglia setup'
 [[ ! `sudo -l | grep ALL | grep NOPASSWD` ]] && ECHO 'no sudo permission'
 [[ `readlink /bin/sh` = dash && -f /bin/bash ]] && ECHO 'bash is not default shell'
-[[ ! `aptitude search maven2 | grep ' maven2' | grep ^i` ]] && ECHO 'no stupid maven2'
 
 if [[ ! -f ~/ibuild/conf/ibuild.conf ]] ; then
 	ECHO 'no ibuild in right place'
 else
-	export IBUILD_SVN_SRV=`grep '^IBUILD_SVN_SRV=' ~/ibuild/conf/ibuild.conf | awk -F'IBUILD_SVN_SRV=' {'print $2'}`
-	export IBUILD_SVN_OPTION=`grep '^IBUILD_SVN_OPTION=' ~/ibuild/conf/ibuild.conf | awk -F'IBUILD_SVN_OPTION=' {'print $2'}`
-	export LOC_REPO_MIRROR_PATH=`grep '^LOC_REPO_MIRROR_PATH=' ~/ibuild/conf/ibuild.conf | awk -F'LOC_REPO_MIRROR_PATH=' {'print $2'}`
-	export LOC_WORKSPACE=`grep '^LOC_WORKSPACE=' ~/ibuild/conf/ibuild.conf | awk -F'LOC_WORKSPACE=' {'print $2'}`
-	export JDK6_PATH=`grep '^JDK6_PATH=' ~/ibuild/conf/ibuild.conf | awk -F'JDK6_PATH=' {'print $2'}`
-	export JDK7_PATH=`grep '^JDK7_PATH=' ~/ibuild/conf/ibuild.conf | awk -F'JDK7_PATH=' {'print $2'}`
+	export IBUILD_SVN_SRV=$(grep '^IBUILD_SVN_SRV=' ~/ibuild/conf/ibuild.conf | awk -F'IBUILD_SVN_SRV=' {'print $2'})
+	export IBUILD_SVN_OPTION=$(grep '^IBUILD_SVN_OPTION=' ~/ibuild/conf/ibuild.conf | awk -F'IBUILD_SVN_OPTION=' {'print $2'})
+	export LOC_REF_REPO_PATH=$(grep '^LOC_REF_REPO_PATH=' ~/ibuild/conf/ibuild.conf | awk -F'LOC_REF_REPO_PATH=' {'print $2'})
+	export LOC_WORKSPACE=$(grep '^LOC_WORKSPACE=' ~/ibuild/conf/ibuild.conf | awk -F'LOC_WORKSPACE=' {'print $2'})
+	export JDK7_PATH=$(grep '^JDK7_PATH=' ~/ibuild/conf/ibuild.conf | awk -F'JDK7_PATH=' {'print $2'})
 	[[ ! `svn ls $IBUILD_SVN_OPTION svn://$IBUILD_SVN_SRV/ispec/ispec` ]] && ECHO 'no right svn setup'
-	[[ ! -d $JDK6_PATH ]] && ECHO 'no jdk1.6'
 	[[ ! -d $JDK7_PATH ]] && ECHO 'no jdk1.7'
 	[[ ! -d $LOC_WORKSPACE ]] && ECHO "no $LOC_WORKSPACE"
-	[[ ! -d $LOC_REPO_MIRROR_PATH ]] && ECHO "no $LOC_REPO_MIRROR_PATH"
+	[[ ! -d $LOC_REF_REPO_PATH ]] && ECHO "no $LOC_REF_REPO_PATH"
 fi
 
 ECHO
