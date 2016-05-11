374c9e96f10ca4f54b6e6a6d87aa94edf02fc3dd
Ding Wei <dingwei@ding.wei>
2016-05-11 10:38:12 -0400

    dw: add docker for monkey

--- setup/setup_ubuntu_build_env.sh
+++ setup/setup_ubuntu_build_env.sh
@@ -85,9 +85,7 @@ sudo aptitude -y install apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >/tmp/docker.list
 sudo cp /tmp/docker.list /etc/apt/sources.list.d/
-sudo apt-get purge lxc-docker
-apt-cache policy docker-engine
-sudo aptitude -y apparmor linux-image-extra-$(uname -r) docker-engine
+sudo aptitude install -y apparmor linux-image-extra-$(uname -r) docker.io
 sudo groupadd docker
 sudo usermod -aG docker $(whoami)
 
