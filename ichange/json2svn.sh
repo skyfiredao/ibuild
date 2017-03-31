ea91f7fa70f80ba2d420805137d5fa644f82382c
Ding Wei <daviding924@outlook.com>
2017-03-31 11:42:20 -0400

    dw: slow steps

--- ichange/json2svn.sh
+++ ichange/json2svn.sh
@@ -174,15 +174,20 @@ do
         echo "$g_revision|$g_id|$g_email|$g_path|$g_project|$g_change_number|$g_patchSet_number|$g_value|$g_ref" >>$TASK_SPACE/itrack/svn/$GERRIT_SRV.$DOMAIN_NAME/$g_branch/$TOWEEK.$g_type
         echo "$g_revision|$g_id|$g_email|$g_path|$g_project|$g_change_number|$g_patchSet_number|$g_value|$g_ref" >>$TASK_SPACE/itrack/svn/$GERRIT_SRV.$DOMAIN_NAME/$g_branch/$TOWEEK.all-change
     fi
+
+    svn cleanup $TASK_SPACE/itrack/svn
+    sleep 0.7
+
     for SVN_ADD in `svn st $TASK_SPACE/itrack/svn | egrep '^\?' | awk -F' ' {'print $2'}`
     do
         svn add -q $SVN_ADD >/dev/null 2>&1
+        sleep 0.7
     done
-    svn cleanup $TASK_SPACE/itrack/svn
-    sleep 0.7
+
     svn ci $ICHANGE_SVN_OPTION -q -F $TASK_SPACE/itrack/$GERRIT_SRV.tmp/$ORDER.log $TASK_SPACE/itrack/svn
     [[ $? = 0 ]] && rm -f $TASK_SPACE/itrack/$GERRIT_SRV.tmp/$ORDER.{json,log}
     sleep 0.7
+
     if [[ `ps aux | grep blame | wc -l` -ge 20 ]] ; then
         export SLEEP=$(expr $(ps aux | grep blame | wc -l) % 7)
         [[ -z $SLEEP ]] && export SLEEP=7
