c59641823e2488c3d26588e189f60e326d80642f
Ding Wei <daviding924@outlook.com>
2017-05-23 10:17:43 -0400

    dw: rewrite rebase_from_xml.sh for more safe

--- misc/rebase_from_xml.sh
+++ misc/rebase_from_xml.sh
@@ -20,56 +20,76 @@ source /etc/bash.bashrc
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH
 export LC_CTYPE=C
 export LC_ALL=C
-export SEED=$RANDOM
+export NOW=$(date +%y%m%d%H%M%S)
 export PWD=$(pwd)
 
 echo -e "$0 <snapshot_manifest.xml> <rebase_project_list.branch>"
-export LOCAL_REPO=$(pwd)
-[[ ! $(ls $LOCAL_REPO/.repo | grep manifests) ]] && exit 1
-export MANIFEST=$1
-[[ ! -f $MANIFEST ]] && exit 1
-export REBASE_PROJ_LIST=$2
-[[ ! -f $REBASE_PROJ_LIST ]] && exit 1
-export BRANCH=$(echo $REBASE_PROJ_LIST | awk -F'.' {'print $2'})
-[[ -z $BRANCH ]] && exit 1
-export BRANCH_NAME=$(basename $BRANCH)
 
+export LOCAL_REPO=$(pwd)
+  [[ ! $(ls $LOCAL_REPO/.repo | grep manifests) ]] && exit 1
+export MANIFEST=$1
+  [[ ! -f $MANIFEST ]] && exit 1
+export REBASE_PROJ_LIST=$2
+  [[ ! -f $REBASE_PROJ_LIST ]] && exit 1
+export BRANCH=$(echo $REBASE_PROJ_LIST | awk -F'.' {'print $2'})
+  [[ -z $BRANCH ]] && exit 1
+export BRANCH_NAME=$(basename $BRANCH)
 export REMOTE_NAME=$(cat $MANIFEST | grep remote= | awk -F'remote="' {'print $2'} | awk -F'"' {'print $1'} | sort -u | head -n1)
+[[ $3 = debug ]] && export DEBUG=echo || export DEBUG=''
+
+SPLIT_LINE()
+{
+ if [[ -z $1 ]] ; then
+    echo -e "=============================================================\n"
+ else
+    echo -e "\n============== $1"
+ fi
+}
+
+RUN_CMD()
+{
+ local RUN_CMD=$1
+ if [[ ! -z $RUN_CMD ]] ; then
+    SPLIT_LINE "$RUN_CMD"
+    time $GM_C_DEBUG_CMD $1
+    local RUN_STATUS=$?
+    CHECK_STATUS $RUN_STATUS "$RUN_CMD"
+ else
+    SPLIT_LINE "Empty Command"
+ fi
+}
+
+CHECK_STATUS()
+{
+ if [[ $1 != 0 ]] ; then
+    ddecho -e "FAILED: $2"
+    SPLIT_LINE
+    echo -e "EXIT $1"
+    exit 1
+ else
+    echo -e "PASSED: $2"
+ fi
+}
 
 for PROJ_NAME in $(cat $REBASE_PROJ_LIST)
 do
     for PROJ_PATH in $(repo list | egrep ": $PROJ_NAME$" | awk -F':' {'print $1'})
     do
-        echo -e "\n------------------------------ $PROJ_PATH"
+        SPLIT_LINE "$PROJ_PATH"
         export PROJ_REV=$(cat $MANIFEST | egrep '"'$PROJ_PATH'"' | awk -F'revision="' {'print $2'} | awk -F'"' {'print $1'})
         pushd $PROJ_PATH >/dev/null 2>&1
-        echo -e ": git remote update --prune"
-        git remote update --prune >/dev/null 2>&1
+        RUN_CMD "git remote update --prune"
         export REMOTE_BRANCH=$(git branch -a | grep remotes | egrep "/$BRANCH$" | awk -F' ' {'print $2'} | sort -u)
-        echo -e ": git reset --hard HEAD"
-        git reset --hard HEAD
-        echo -e ": git checkout -b rebase-$SEED $PROJ_REV"
-        git checkout -b rebase-$SEED $PROJ_REV
-        echo -e ": git checkout -b $BRANCH_NAME $REMOTE_BRANCH"
-        git checkout -b $BRANCH_NAME $REMOTE_BRANCH
-        echo -e ": git rebase rebase-$SEED"
-        git rebase rebase-$SEED
-        if [[ $? != 0 ]] ; then
-            echo -e "\n>>>>>>> issue project: $PROJ_PATH\n"
-            echo -e ": git rebase --abort"
-            git rebase --abort
-        fi
-        echo -e "git push -f $REMOTE_NAME $BRANCH_NAME"
-        git push -f $REMOTE_NAME $BRANCH_NAME
-        [[ $? != 0 ]] && echo -e "\n>>>>>>> issue project: $PROJ_PATH\n"
+        RUN_CMD "git reset --hard HEAD"
+        RUN_CMD "git checkout -b $BRANCH_NAME $REMOTE_BRANCH"
+        RUN_CMD "git rebase $PROJ_REV || git rebase --abort"
+        RUN_CMD "$DEBUG git push $REMOTE_NAME $BRANCH_NAME-$NOW"
+        echo $PROJ_NAME >>$REBASE_PROJ_LIST.done
+        cat $REBASE_PROJ_LIST | grep -v "^$PROJ_NAME$" >$REBASE_PROJ_LIST.tmp
+        cp $REBASE_PROJ_LIST.tmp $REBASE_PROJ_LIST
         popd >/dev/null 2>&1
     done
 done
-
-echo -e ": repo abandon $REMOTE_NAME"
-repo abandon $REMOTE_NAME
-echo -e ": repo abandon rebase-$SEED"
-repo abandon rebase-$SEED
 
 for REPO_BRANCH in $(repo branch | grep '|' |awk -F'|' {'print $1'} | sed 's/*//g')
 do
