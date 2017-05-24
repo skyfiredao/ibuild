be1c7824bcc74ec91f225e04572dd9d23aca3bcf
Ding Wei <daviding924@outlook.com>
2017-05-24 08:10:15 -0400

    dw: add docker-ce

--- setup/setup_ubuntu_build_env.sh
+++ setup/setup_ubuntu_build_env.sh
@@ -77,8 +77,9 @@ if [[ `echo $RUN_OPTION | egrep 'docker'` ]] ; then
     $DEBUG aptitude -y install apt-transport-https ca-certificates
     $DEBUG apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
     echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >/tmp/docker.list
+    echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu trusty stable" >>/tmp/docker.list
     $DEBUG cp /tmp/docker.list /etc/apt/sources.list.d/
-    $DEBUG aptitude install -y apparmor linux-image-extra-$(uname -r) docker.io
+    $DEBUG aptitude install -y apparmor linux-image-extra-$(uname -r) docker-ce
     $DEBUG groupadd docker
     $DEBUG usermod -aG docker $(whoami)
 fi
