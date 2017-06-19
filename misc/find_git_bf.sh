45ca6c1b4712e9915b1e67a98e0fec781e28dfe9
Ding Wei <daviding924@outlook.com>
2017-06-19 14:42:33 -0400

    dw: add git repo support

--- misc/find_git_bf.sh
+++ misc/find_git_bf.sh
@@ -2,9 +2,18 @@
 IFS=$'\n';
  
 export GIT_TOP=$(git rev-parse --show-toplevel)
+
+if [[ -d $GIT_TOP/.git/objects/pack ]] ; then
+    export OBJ_PACK_PATH=$GIT_TOP/.git/objects/pack
+elif [[ -d $(pwd)/objects/pack ]] ; then
+    export OBJ_PACK_PATH=$(pwd)/objects/pack
+else
+    echo "Cannot find objects/pack path"
+    exit 1
+fi
  
 OUTPUT="Size(KB),Git(KB),SHA1,URL"
-for OBJ in $(git verify-pack -v $GIT_TOP/.git/objects/pack/pack-*.idx | grep -v chain | sort -k3nr | head)
+for OBJ in $(git verify-pack -v $OBJ_PACK_PATH/pack-*.idx | grep -v chain | sort -k3nr | head)
 do
     SIZE=$(($(echo $OBJ | cut -f 5 -d ' ')/1024))
     COMPRESSED_SIZE=$(($(echo $OBJ | cut -f 6 -d ' ')/1024))
