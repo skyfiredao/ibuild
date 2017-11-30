8e10de75e32c38b3121295505ce83c22d95611d6
Ding Wei <daviding924@outlook.com>
2017-11-30 15:26:02 -0500

    dw: add pkill when reboot

--- setup/reboot.sh
+++ setup/reboot.sh
@@ -64,6 +64,8 @@ if [[ -e /dev/shm/spec.build ]] ; then
     export SPEC_TIME=$(stat /dev/shm/spec.build | grep Modify | awk -F' ' {'print $2'} | awk -F'-' {'print $3'})
     export LOAD_NOW=$(w | grep average | awk -F'average: ' {'print $2'} | awk -F'.' {'print $1'})
     if [[ $(echo $(date +%d) - $SPEC_TIME | bc) -ge 2 ]] ; then
+        pkill -9 java
+        pkill -9 aapt
         rm -f /dev/shm/spec.build
         REBOOT_STEP
     fi
